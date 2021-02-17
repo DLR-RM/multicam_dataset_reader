4a977ba234978c41aa51c299f2e74e0442905c5a
Marco Sewtz <marco.sewtz@dlr.de>
2021-02-17 12:56:02 +0100

    feat: add DatasetReader class

---
+++ src/dataset_reader/DatasetReader.cc
@@ -0,0 +1,5 @@
+//
+// Created by sewt_ma on 2/17/21.
+//
+
+#include "DatasetReader.h"
---
+++ src/dataset_reader/DatasetReader.h
@@ -0,0 +1,14 @@
+//
+// Created by sewt_ma on 2/17/21.
+//
+
+#ifndef MULTICAM_DATASET_DATASETREADER_H
+#define MULTICAM_DATASET_DATASETREADER_H
+
+
+class DatasetReader{
+
+};
+
+
+#endif //MULTICAM_DATASET_DATASETREADER_H
