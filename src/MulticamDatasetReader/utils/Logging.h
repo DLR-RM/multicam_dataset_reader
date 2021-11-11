b583c9d4680ddbde93814b98b59ef323da47462a
Marco Sewtz <marco.sewtz@dlr.de>
2021-11-11 15:28:56 +0100

    feat: add logging

---
+++ src/MulticamDatasetReader/utils/Logging.h
@@ -0,0 +1,32 @@
+//
+// Created by sewt_ma on 7/30/21.
+//
+
+#ifndef MULTICAM_DATASET_LOGGING_H
+#define MULTICAM_DATASET_LOGGING_H
+
+#include <string>
+using message_type_t = std::string;
+
+namespace MD::Log{
+
+enum class log_level_value{
+	error = 1,
+	warn  = 2,
+	info  = 3,
+	debug = 4,
+	all   = 5
+};
+
+void debug(const message_type_t& msg, const std::string& line_ending="\n\r");
+
+void info(const message_type_t& msg, const std::string& line_ending="\n\r");
+
+void warn(const message_type_t& msg, const std::string& line_ending="\n\r");
+
+void error(const message_type_t& msg, const std::string& line_ending="\n\r");
+
+
+}
+
+#endif //MULTICAM_DATASET_LOGGING_H
