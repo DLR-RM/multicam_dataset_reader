805048498c3127b388ef1f5cea52f214227b57b8
Marco Sewtz <marco.sewtz@dlr.de>
2021-11-11 11:47:11 +0100

    feat: add Image

---
+++ src/MulticamDatasetReader/core/Image.h
@@ -0,0 +1,18 @@
+//
+// Created by sewt_ma on 11/11/21.
+//
+
+#ifndef MDR_IMAGE_H
+#define MDR_IMAGE_H
+
+#include <MulticamDatasetReader/utils/Filesystem.h>
+
+namespace MDR{
+
+class Image {
+
+}
+
+}
+
+#endif //MDR_IMAGE_H
