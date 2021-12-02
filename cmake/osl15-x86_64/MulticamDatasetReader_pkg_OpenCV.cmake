895752d03253ca5b0d010060a394668e879212bd
Marco Sewtz <marco.sewtz@dlr.de>
2021-12-02 14:52:07 +0100

    feat: added highgui

--- cmake/osl15-x86_64/MulticamDatasetReader_pkg_OpenCV.cmake
+++ cmake/osl15-x86_64/MulticamDatasetReader_pkg_OpenCV.cmake
@@ -1,7 +1,7 @@
 
 if(NOT MulticamDatasetReader_OpenCV_LOADED)
     message(STATUS "> searching for cmake package 'OpenCV'")
-    find_package(OpenCV REQUIRED COMPONENTS core imgcodecs)
+    find_package(OpenCV REQUIRED COMPONENTS core imgcodecs highgui)
     set(MulticamDatasetReader_OpenCV_LOADED True)
 endif()
         
