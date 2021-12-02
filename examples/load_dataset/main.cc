ffc90cbaa1fab17f047737e7deb872773bef48f1
Marco Sewtz <marco.sewtz@dlr.de>
2021-12-02 14:52:21 +0100

    feat: view rgb

--- examples/load_dataset/main.cc
+++ examples/load_dataset/main.cc
@@ -4,18 +4,35 @@
 
 #include <MulticamDatasetReader/core/Device.h>
 
+#include <opencv2/highgui.hpp>
+
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main(int argc, char** argv){
 	MDR::Device dev("/home_local/datasets/mcd4/mcd4_s0_r0/front");
 
-	std::cout << std::to_string(dev.get_first_sampling_time()) << std::endl;
-	std::cout << std::to_string(dev.get_last_sampling_time()) << std::endl;
+	double t = dev.get_first_sampling_time(MDR::SensorType::rgb);
+	double e = dev.get_last_sampling_time(MDR::SensorType::rgb);
 
-	std::cout << std::endl;
+	double last_t = 0;
 
-	std::cout << std::to_string(dev.get_time_of_next_measurement(dev.get_first_sampling_time())) << std::endl;
-	std::cout << std::to_string(dev.get_time_of_next_measurement(dev.get_first_sampling_time()) - dev.get_first_sampling_time()) << std::endl;
+	MDR::DeviceMeasurements last_meas;
+	while(t < e){
+		auto meas = dev.get(t);
+
+		std::cout << std::to_string(t) << std::to_string(last_t - t) << std::endl;
+		//std::this_thread::sleep_for(std::chrono::milliseconds(250));
+
+		cv::imshow("RGB", meas.rgb->matrix());
+		cv::waitKey(66);
+
+		last_meas = meas;
+		last_t    = t;
+		t = dev.get_time_of_next_measurement(t, MDR::SensorType::rgb);
+	}
+
 
 	return EXIT_SUCCESS;
 }
