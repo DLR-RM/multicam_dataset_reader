6407b2782ca8b4b72d30d6314765f82f32e45186
Marco Sewtz <marco.sewtz@dlr.de>
2021-12-06 13:55:54 +0100

    chore: is_loaded mark as override

--- src/MulticamDatasetReader/core/InertiaMeasurement.h
+++ src/MulticamDatasetReader/core/InertiaMeasurement.h
@@ -19,7 +19,7 @@ public:
 	bool can_lazy_load() const override { return false;	}
 	void load() override;
 	void unload() override;
-	bool is_loaded() const { return true; };
+	bool is_loaded() const override { return true; };
 
 private:
 	std::array<double, 3> m_data;
