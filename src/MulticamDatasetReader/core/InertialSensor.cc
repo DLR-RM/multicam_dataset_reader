cf1fac8780b6f08f01aa883b0467defd3608a992
Marco Sewtz <marco.sewtz@dlr.de>
2021-12-06 13:55:35 +0100

    feat: parse sensor file implementation

--- src/MulticamDatasetReader/core/InertialSensor.cc
+++ src/MulticamDatasetReader/core/InertialSensor.cc
@@ -76,6 +76,41 @@ void MDR::InertialSensor::parse_frames_file() {
 	fp.close();
 }
 
+void InertialSensor::parse_sensor_file() {
+	// check for sensor file
+	if(not fs::exists(get_sensor_file())){
+		Log::error("Could not find " + get_sensor_file().string());
+		return;
+	}
+
+	// open file
+	std::ifstream fp(get_sensor_file().string());
+
+	// read line-by-line
+	std::string line;
+	while(std::getline(fp, line)){
+		// skip comments and empty lines
+		if(line.rfind('#', 0) != 0 and not line.empty()){
+			const auto& parts = Utility::split(line, ';');
+
+			// pose has 7 elements
+			if(parts.size() == 7){
+				double px = std::stod(parts[0]);
+				double py = std::stod(parts[1]);
+				double pz = std::stod(parts[2]);
+				double qx = std::stod(parts[3]);
+				double qy = std::stod(parts[4]);
+				double qz = std::stod(parts[5]);
+				double qw = std::stod(parts[6]);
+				set_pose(Utility::create_pose_matrix(px, py, pz, qx, qy, qz, qw));
+			}
+		}
+	}
+
+	// close file
+	fp.close();
+}
+
 bool InertialSensor::check_if_sensor_exists(const boost::filesystem::path &path, bool is_accel) {
 	if(is_accel){
 		if(not fs::exists(path/Globals::frames_file_accel)){
